81a90e66c3e5859b959e6c8cbec3c6730da65e70
happy531 <jedrzejwesolowski177@gmail.com>
2022-03-24 18:25:37 +0100

    wrap _app with AuthContext.Provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,24 @@
-import '../styles/globals.scss'
+import { AuthContext } from "../context/auth-context";
+import { useAuth } from "../hooks/auth-hook";
+
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const { token, login, logout, userId } = useAuth();
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: !!token,
+        token: token,
+        userId: userId,
+        login: login,
+        logout: logout,
+      }}
+    >
+      <Component {...pageProps} />
+    </AuthContext.Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
