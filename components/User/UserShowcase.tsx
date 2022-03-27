18444c85702ecd2e8232025293cf0e033e6fc69c
happy531 <jedrzejwesolowski177@gmail.com>
2022-03-27 15:34:49 +0200

    add User Profile Page and UserShowcase component

---
+++ components/User/UserShowcase.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Avatar from "@mui/material/Avatar";
+import Grid from "@mui/material/Grid";
+
+interface Props {
+  name: string;
+  image: string;
+}
+
+const UserShowcase: React.FC<Props> = ({ name, image }) => {
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="row"
+      alignItems="center"
+      justifyContent="space-around"
+      sx={{
+        marginLeft: "auto",
+        marginRight: "auto",
+        width: "15%",
+        backgroundColor: "lightblue",
+        border: "2px solid gray",
+      }}
+    >
+      <Avatar
+        alt={name}
+        src={`http://localhost:5000/${image}`}
+        sx={{ width: 100, height: 100, border: "1px solid black" }}
+      />
+      <h2>{name}</h2>
+    </Grid>
+  );
+};
+
+export default UserShowcase;
---
+++ pages/user/[userId].tsx
@@ -0,0 +1,33 @@
+import Head from "next/head";
+
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import PlacesList from "../../components/Place/PlacesList/PlacesList";
+import axios from "../../axios/axios";
+import UserShowcase from "../../components/User/UserShowcase";
+
+export default function UserProfile({ items, user }) {
+  return (
+    <>
+      <Head>
+        <title>Places - {user.name}</title>
+        <meta name="user profile" content="User profile" />
+      </Head>
+      <main>
+        <Header />
+        <UserShowcase name={user.name} image={user.image} />
+        <PlacesList items={items} />
+        <Footer />
+      </main>
+    </>
+  );
+}
+
+export async function getServerSideProps({ params }) {
+  const { data: placeData } = await axios.get(`/places/user/${params.userId}`);
+
+  const { data: userData } = await axios.get(`/users/${params.userId}`);
+  return {
+    props: { items: placeData.places, user: userData.user },
+  };
+}
