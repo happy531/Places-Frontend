9cbfea0c579e202cab10afe78a7f753223415ce8
happy531 <jedrzejwesolowski177@gmail.com>
2022-04-02 21:18:48 +0200

    delete utils

--- util/validators.ts
+++
@@ -1,9 +0,0 @@
-export function validateEmail(email: string): boolean {
-  const re =
-    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
-}
-
-export function validateMinLength(word: string, lowerBound: number): boolean {
-  return word.length >= lowerBound;
-}
