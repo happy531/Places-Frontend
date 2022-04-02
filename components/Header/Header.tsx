b77d1ec004f07b74c28788ab214b38d9d98a60d7
happy531 <jedrzejwesolowski177@gmail.com>
2022-04-02 16:23:08 +0200

    making Header responsive

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,13 +1,29 @@
-import React from "react";
-import Link from "next/link";
+import React, { useState, useEffect } from "react";
 import { useAuth } from "../../hooks/auth-hook";
+import Link from "next/link";
 
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import CameraIcon from "@mui/icons-material/PhotoCamera";
 
 import classes from "./Header.module.scss";
 
 const Header: React.FC = () => {
+  const [shrink, setShrink] = useState<boolean>(false);
+
+  useEffect(() => {
+    const shrinkHeader = () => {
+      if (document.documentElement.scrollTop > 100) {
+        setShrink(true);
+      } else {
+        setShrink(false);
+      }
+    };
+    window.addEventListener("scroll", shrinkHeader);
+    return () => {
+      window.removeEventListener("scroll", shrinkHeader);
+    };
+  }, []);
+
   const { userId, token, logout } = useAuth();
 
   const handleLogout = () => {
@@ -15,58 +31,43 @@ const Header: React.FC = () => {
   };
 
   return (
-    <AppBar>
-      <Toolbar className={`${classes.header}`}>
-        <div
-          style={{
-            width: "100%",
-            height: "100%",
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-          }}
-        >
+    <div className={`${classes.header} ${shrink ? classes.shrink : ""}`}>
+      <div className={`${classes.header__wrap}`}>
+        <div className={classes.logo}>
           <CameraIcon sx={{ mr: 2 }} />
           <Typography variant="h6" color="inherit" noWrap>
             <Link href="/">Places App</Link>
           </Typography>
         </div>
-        <Typography
-          variant="h6"
-          color="inherit"
-          noWrap
-          sx={{ minWidth: "20%" }}
-        >
-          <ul className={classes.header__nav}>
-            {!token && (
-              <li>
-                <Link href={"/auth/signin"}>Sign in</Link>
-              </li>
-            )}
-            {!token && (
-              <li>
-                <Link href={"/auth/signup"}>Sign up</Link>
-              </li>
-            )}
-            {token && (
-              <li>
-                <Link href={"/place/new"}>Share place</Link>
-              </li>
-            )}
-            {token && (
-              <li>
-                <Link href={`/user/${userId}`}>Your Places</Link>
-              </li>
-            )}
-            {token && (
-              <li onClick={handleLogout}>
-                <Link href={`/`}>Logout</Link>
-              </li>
-            )}
-          </ul>
-        </Typography>
-      </Toolbar>
-    </AppBar>
+        <ul className={classes.header__nav}>
+          {!token && (
+            <li>
+              <Link href={"/auth/signin"}>Sign in</Link>
+            </li>
+          )}
+          {!token && (
+            <li>
+              <Link href={"/auth/signup"}>Sign up</Link>
+            </li>
+          )}
+          {token && (
+            <li>
+              <Link href={"/place/new"}>Share place</Link>
+            </li>
+          )}
+          {token && (
+            <li>
+              <Link href={`/user/${userId}`}>Your Places</Link>
+            </li>
+          )}
+          {token && (
+            <li onClick={handleLogout}>
+              <Link href={`/`}>Logout</Link>
+            </li>
+          )}
+        </ul>
+      </div>
+    </div>
   );
 };
 
