b248cede53524c04c33f6981a168ddccebe92331
happy531 <jedrzejwesolowski177@gmail.com>
2022-04-03 11:53:00 +0200

    add _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang={"en"}>
+        <Head />
+        <body>
+          <div className="delete_overlay" />
+          <div className="edit_overlay" />
+          <div className="map_overlay" />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
