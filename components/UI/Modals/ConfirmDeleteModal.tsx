656eeb779ca41731342577bbac0374d5b0aee79b
happy531 <jedrzejwesolowski177@gmail.com>
2022-04-08 23:44:18 +0200

    add error handling and loading spinner to ConfirmDeleteModal.tsx

--- components/UI/Modals/ConfirmDeleteModal.tsx
+++ components/UI/Modals/ConfirmDeleteModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
@@ -8,6 +8,8 @@ import DialogTitle from "@mui/material/DialogTitle";
 import axios from "../../../axios/axios";
 import { useAuth } from "../../../hooks/auth-hook";
 import { useRouter } from "next/router";
+import { Alert } from "@mui/material";
+import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 
 interface Props {
   open: boolean;
@@ -23,17 +25,25 @@ const ConfirmDeleteModal: React.FC<Props> = ({
   const { token } = useAuth();
   const router = useRouter();
 
+  const [loading, setLoading] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>(null);
+
   const handleDeletePlace = async () => {
     try {
+      setErrorMessage(null);
+      setLoading(true);
+
       await axios.delete(`/places/${placeId}`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       });
 
-      // handleCloseDeleteModal();
       router.reload();
-    } catch (err) {}
+    } catch (err) {
+      if (err.response) setErrorMessage(err.response.data.message);
+      setLoading(false);
+    }
   };
 
   return (
@@ -50,12 +60,22 @@ const ConfirmDeleteModal: React.FC<Props> = ({
         <DialogContentText id="alert-dialog-description">
           Are you sure you want delete this place? It will be irreversible.
         </DialogContentText>
+        {errorMessage && (
+          <Alert severity="error" style={{ width: "100%", margin: 5 }}>
+            {errorMessage}
+          </Alert>
+        )}
       </DialogContent>
       <DialogActions>
-        <Button onClick={handleClose}>Cancel</Button>
-        <Button onClick={handleDeletePlace} autoFocus>
-          Delete
-        </Button>
+        {loading && <LoadingSpinner />}
+        {!loading && (
+          <>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handleDeletePlace} autoFocus>
+              Delete
+            </Button>
+          </>
+        )}
       </DialogActions>
     </Dialog>
   );
